e9d75055c2cf0358698044b194749c93e1752ce9
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2024-08-07 04:47:40 +0600

    remove: Banner component

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,6 +1,5 @@
 import clsx from 'clsx';
 
-import Banner from 'components/banner';
 import LoginForm from 'components/form/login.form';
 
 import styles from '../styles.module.scss';
@@ -11,9 +10,7 @@ export default function SignIn() {
       <div className={clsx('wrapper', styles.wrapper)}>
         <div className={clsx('container', styles.container)}>
           <div className={styles.content_container}>
-            <Banner>
-              <LoginForm />
-            </Banner>
+            <LoginForm />
           </div>
         </div>
       </div>
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,6 +1,5 @@
 import clsx from 'clsx';
 
-import Banner from 'components/banner';
 import RegisterForm from 'components/form/register.form';
 
 import styles from '../styles.module.scss';
@@ -11,9 +10,7 @@ export default function SignUp() {
       <div className={clsx('wrapper', styles.wrapper)}>
         <div className={clsx('container', styles.container)}>
           <div className={styles.content_container}>
-            <Banner>
-              <RegisterForm />
-            </Banner>
+            <RegisterForm />
           </div>
         </div>
       </div>
